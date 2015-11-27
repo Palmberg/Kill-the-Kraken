5cd41614c3dbbd2a7a834f76adc901db2137168a
Palmberg <robinpa@kth.se>
2015-11-27 14:28:57 +0100

    Adding touch controls
    Now we have a joystick

--- Kraken/Assets/Scripts/WorldMain.cs
+++ Kraken/Assets/Scripts/WorldMain.cs
@@ -7,7 +7,7 @@ public class WorldMain : MonoBehaviour {
     private GameObject boat;
     private Vector3 yMovement = new Vector3(0f, 1f, 0f);
     private Vector3 xMovement = new Vector3(1f, 0f, 0f);
-    private float maxRange = 10f;
+    private float maxRange = 4f;
 
     // Use this for initialization
     void Start()
@@ -21,6 +21,37 @@ public class WorldMain : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        if (Input.touchCount > 0)
+        {
+            // Get movement of the finger
+            Vector3 touchPosition = Input.GetTouch(0).deltaPosition;
+            //if (boat.transform.position.y <= maxRange && boat.transform.position.x <= maxRange && boat.transform.position.y >= -maxRange && boat.transform.position.x >= -maxRange)
+            //{
+            //    // Move object across XY plane
+            //    boat.transform.position += touchPosition / 10;
+            //}
+            //else
+            //{
+            //    boat.transform.position -= touchPosition / 10; 
+            //}
+            if (touchPosition.x >= 0 && boat.transform.position.x <= maxRange)
+            {
+                boat.transform.position += new Vector3((touchPosition.x / 10),0f,0f);
+            }
+            else if (touchPosition.x < 0 && boat.transform.position.x >= -maxRange)
+            {
+                boat.transform.position += new Vector3((touchPosition.x / 10), 0f, 0f);
+            }
+            if (touchPosition.y >= 0 && boat.transform.position.y <= maxRange)
+            {
+                boat.transform.position += new Vector3(0f, (touchPosition.y / 10), 0f);
+            }
+            else if (touchPosition.y < 0 && boat.transform.position.y >= -maxRange)
+            {
+                boat.transform.position += new Vector3(0f, (touchPosition.y / 10), 0f);
+            }
+
+        }
         if (Input.GetKey(KeyCode.UpArrow))
         {
             if (boat.transform.position.y <= maxRange)
